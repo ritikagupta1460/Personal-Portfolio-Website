7ee40d69a8c648182f0b7ca4d3c373f494bf5a6c
ritikagupta1460 <160583140+ritikagupta1460@users.noreply.github.com>
2024-08-07 23:27:30 +0530

    Add files via upload

---
+++ mainScript.js
@@ -0,0 +1,9 @@
+/*import Typed from 'https://cdn.jsdelivr.net/npm/typed.js@2.0.12/dist/typed.esm.js';*/
+
+var typed=new Typed(".text", {
+    strings: ["Frontend Developer", "Aspirant","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
